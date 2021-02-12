d7329cf64e57f76492a4ff03abaa33b5b3ca13b8
k1863 <57972610+k1863@users.noreply.github.com>
2021-02-12 00:21:08 -0300

    implemented logic for button rendering on play, pause, resume and stop clicks, also implemented respective function callbacks that increment time in 1000ms, pause time increments, resume time increments  and converted the time to seconds, minutes and hours

--- src/TimerPage/TimerPage.jsx
+++ src/TimerPage/TimerPage.jsx
@@ -1,23 +1,62 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "../sass/styles.scss";
 import StopBtn from "../svg/StopBtn.svg";
 import PlayBtn from "../svg/PlayBtn.svg";
+import ResumeBtn from "../svg/ResumeBtn.svg";
 import PauseBtn from "../svg/PauseBtn.svg";
+import useTimer from "../utils/useTimer.js";
+import { formatTime } from "../utils/utils.js";
 
 function TimerPage(props) {
+  const {
+    timer,
+    isActive,
+    isPaused,
+    handleStart,
+    handlePause,
+    handleResume,
+    handleReset,
+  } = useTimer(0);
+
   return (
     <div className="timer-page">
       <h2 className="header-secondary">Stopwatch</h2>
       <span className="timer-page__time-wrapper">
-        <h1 className="header-primary">00:00:00</h1>
+        <h1 className="header-primary">{formatTime(timer)}</h1>
       </span>
       <div className="timer-page__btns">
         <span className="timer-page__btn-wrapper1">
-          <img className="stop-btn" src={StopBtn} alt="stop" />
+          <img
+            className="stop-btn"
+            onClick={handleReset}
+            src={StopBtn}
+            alt="stop"
+            disabled={!isActive}
+          />
         </span>
         <span className="timer-page__btn-wrapper2">
-          <img className="play-btn" src={PlayBtn} alt="stop" />
-          <img className="pause-btn" src={PauseBtn} alt="stop" />
+          {!isActive && !isPaused ? (
+            <img
+              className="play-btn"
+              onClick={handleStart}
+              src={PlayBtn}
+              alt="stop"
+            />
+          ) : isPaused ? (
+            <img
+              className="pause-btn"
+              onClick={handlePause}
+              src={PauseBtn}
+              alt="pause"
+            />
+          ) : (
+            <img
+              className="pause-btn"
+              onClick={handleResume}
+              src={ResumeBtn}
+              alt="resume"
+            />
+          )}
         </span>
       </div>
     </div>
